d365e07fbfdea5ab2a04b0ea1df758e35c76eaa5
toshitalol <dogratoshita24@gmail.com>
2025-06-11 23:01:37 +0530

    a

---
+++ firstcode.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+cout<<" hi vani \nim ken";
+int age=14;
+cout<<age;
+return 0;
+} 
---
+++ input.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+cout<<"enter a";
+cin>> a;
+cout<<"enter b";
+cin>>b;
+cout<<"the sum is"<<(a+b);
+cout<<"\nthe difference is"<<(a-b);
+cout<<"\nthe product is"<<(a*b);
+cout<<"\nthe division is"<<(a/b);
+cout<<"\nthe remainder is"<<(a%b);
+
+
+
+
+return 0;
+
+
+
+}
